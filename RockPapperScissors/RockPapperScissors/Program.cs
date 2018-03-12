1f2637a9700599fa8980e61df1fa4f324168c21b
Flores <0636730@students.dadeschools.net>
2018-03-12 12:02:05 -0400

    starting project

---
+++ RockPapperScissors/RockPapperScissors/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 0636730
+ * Date: 3/12/2018
+ * Time: 11:55 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RockPapperScissors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
